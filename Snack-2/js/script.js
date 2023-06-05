50acb3e366e5865d96a18a66795ab8b8e50744a3
robertobenfini <roberto.benfini@gmail.com>
2023-06-05 13:10:55 +0200

    [ADD] boiler-snack-3-4

--- Snack-2/js/script.js
+++ Snack-2/js/script.js
@@ -1,45 +1,42 @@
 //array studenti
 let studenti = [
-    { 'id': '213',
+    {   'id': '213',
         'nome': 'Marco della Rovere',
         'grado': '78',
     },
-    { 'id': '110',
+    {   'id': '110',
         'nome': 'Paola Cortellesa',
         'grado': '96',
     },
-    { 'id': '250',
+    {   'id': '250',
         'nome': 'Andrea Mantegna',
         'grado': '48',
     },
-    { 'id': '145',
+    {   'id': '145',
         'nome': 'Gaia Borromini',
         'grado': '74',
     },
-    { 'id': '196',
+    {   'id': '196',
         'nome': 'Luigi Grimaldello',
         'grado': '68',
     },
-    { 'id': '102',
+    {   'id': '102',
         'nome': 'Piero della Francesca',
         'grado': '50',
     },
-    { 'id': '120',
+    {   'id': '120',
         'nome': 'Francesca della Polenta',
         'grado': '84',
     },
 ];
+console.log(studenti)
 
 //nome studenti in maiuscolo
-let nome_up = studenti.map((elem) => {
-
-    let new_nome = elem.nome.toUpperCase() 
-    return new_nome
-})
+let nome_up = studenti.map((elem) => elem.nome.toUpperCase())
 console.log(nome_up)
 
 //filtro l'array con il grado superiore al 70
-let voti_70 = studenti.filter((item) => { if(item.grado > 70)return true})
+let voti_70 = studenti.filter((item) => (item.grado > 70))
 console.log(voti_70)
 
 //filtro l'array con il grado superiore al 70 e id superiore a 120
